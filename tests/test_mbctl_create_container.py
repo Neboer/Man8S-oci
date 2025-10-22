478e794570c413d9f53e45508f712b1fb3cd5184
Neboer <rubinposter@gmail.com>
2025-10-22 09:14:20 +0800

    新建test。现在有bug mbctl不会主动创建主机中对应的挂载点；我们希望修改mbctl为向用户提问挂载目标并设置。

--- tests/test_mbctl_create_container.py
+++ tests/test_mbctl_create_container.py
@@ -8,8 +8,14 @@ mirror_list = [
 ]
 
 def test_pull_oci_image_and_create_container():
+    # pull_oci_image_and_create_container(
+    #     oci_image_url="gcr.nju.edu.cn/google-containers/alpine-with-bash@sha256:0955672451201896cf9e2e5ce30bec0c7f10757af33bf78b7a6afa5672c596f5",
+    #     container_name="TestContainer",
+    #     container_template="network_isolated",
+    # )
+
     pull_oci_image_and_create_container(
-        oci_image_url="gcr.nju.edu.cn/google-containers/alpine-with-bash@sha256:0955672451201896cf9e2e5ce30bec0c7f10757af33bf78b7a6afa5672c596f5",
-        container_name="TestContainer",
+        oci_image_url="ghcr.nju.edu.cn/bitwarden/self-host:latest",
+        container_name="TestBWContainer",
         container_template="network_isolated",
     )
