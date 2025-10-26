282ebdd4c5ef235df35c5362a2765ea2ee2cf8b4
Neboer <rubinposter@gmail.com>
2025-10-26 19:18:13 +0800

    修复bug，挂载与nspawn配置的正反问题，删除 volatile 挂载选项（因为不支持idmap）

--- src/mbctl/config_formats/nspawn_config.py
+++ src/mbctl/config_formats/nspawn_config.py
@@ -7,13 +7,17 @@ class NspawnConfig(SystemdUnitINIConfig):
     def add_environment_var(self, key: str, value: str):
         self.add_key_value("Exec", "Environment", f'"{key}={value}"')
 
-    # 单个挂载: src, dst[, opts]
-    def add_bind_mount(self, src: str, dst: str, opts: str | None = None):
-        bind_entry = f"{src}:{dst}" + (f":{opts}" if opts else "")
+    # 单个挂载: mount_point, mount_target
+    ## 注意，实际写入bind的mount_point与mount_target是相反的。
+    ## mount_point 指的是容器内的路径，mount_target 指的是宿主机的路径。
+    ## 为什么这里要反过来？ 因为 systemd-nspawn 的 Bind 配置项的格式是 host_path:container_path 。
+    ## 而我们在 Man8SContainerInfo 中定义的挂载点是 mount_point -> mount_target ，
+    def add_bind_mount(self, mount_point: str, mount_target: str, opts: str | None = None):
+        bind_entry = f"{mount_target}:{mount_point}" + (f":{opts}" if opts else "")
         self.add_key_value("Files", "Bind", bind_entry)
-    
-    def add_bind_mount_idmap(self, src: str, dst: str):
-        return self.add_bind_mount(src, dst, "idmap")
+
+    def add_bind_mount_idmap(self, mount_point: str, mount_target: str):
+        return self.add_bind_mount(mount_point, mount_target, "idmap")
 
     # 设置工作目录和启动命令
     def set_working_directory(self, path: str):
