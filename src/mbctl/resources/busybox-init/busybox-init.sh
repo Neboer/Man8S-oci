ba202ade8e972f46e7f06f44f3b10bf746d4b5fb
Neboer <rubinposter@gmail.com>
2025-10-27 11:57:22 +0800

    修复环境变量加载的问题

--- src/mbctl/resources/busybox-init/busybox-init.sh
+++ src/mbctl/resources/busybox-init/busybox-init.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # A minimal init script for BusyBox-based containers.
-source /man8env.env
 source /sbin/busybox-execute.sh
+source /sbin/busybox-load-envs.sh
 source /sbin/busybox-autoconfig-networking.sh
 source /sbin/yggdrasil-config-ipaddr.sh
 source /sbin/application.sh
